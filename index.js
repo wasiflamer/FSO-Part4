7ca77c640ab56b62df74d724703f1ea5606d4c06
wasiflamer <wasiflamer@gmail.com>
2024-01-11 05:39:52 +0500

    refactored a little

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+// exports
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -40,7 +44,6 @@ app.post("/api/blogs", async (request, response) => {
   }
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
