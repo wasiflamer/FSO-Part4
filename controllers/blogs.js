95b64422733d42e1872ce7b57df549205630d2ad
wasiflamer <wasiflamer@gmail.com>
2024-02-07 21:14:10 +0500

    4.18 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,16 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 // get all blogs | router
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user");
@@ -25,7 +35,11 @@ blogsRouter.get("/:id", (request, response, next) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
@@ -34,6 +48,12 @@ blogsRouter.post("/", async (request, response) => {
     likes: body.likes,
     user: user.id,
   });
+
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: "token invalid",
+    });
+  }
 
   if (!body.title) {
     response.status(400).json(blog);
